aefd9c87b87b3ad679036c459bc440bfe9f5296c
elmatiino <elmatiino@gmail.com>
2026-07-28 21:06:06 +0330

    توضیح کوتاه تغییر

--- app/panel/aruz/page.tsx
+++ app/panel/aruz/page.tsx
@@ -131,28 +131,7 @@ function page() {
             </div>
           </div>
           <div className=" bg-secondary p-4 mt-3 rounded-xl">
-            <div className=" text-center">
-              ای گلبن نادیده دی اصل تو چه وصل تو کی / با بوی مشک و رنگ می از
-              گلستان کیستی
-            </div>
             <div>
-              <div className=" flex items-center gap-x-2">
-                <div className=" size-6 rounded-full text-red-500 border-2 flex items-center justify-center border-red-500">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-4"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-                <div className=" text-muted-foreground">پاسخ شما:</div>
-              </div>
               <OrouzDemo />
             </div>
           </div>
